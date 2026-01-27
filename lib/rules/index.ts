0f6771f307a3e96275fa684cad1564cb7348926b
Pavel Vinogradov <pavel.vinogradov@mpstats.io>
2026-01-27 21:11:16 +0300

    chore: main Скорректирован экспорт правил из dist

---
+++ lib/rules/index.ts
@@ -0,0 +1,15 @@
+import addVueExtension from './add-vue-extension.js';
+import importEntitesByColumnOrLine from './import-entities-by-column-or-line.js';
+import preferTrueAttributeShorthand from './prefer-true-attribute-shorthand.js';
+import useShortestAlias from './use-shortest-alias.js';
+import variableEntitiesByColumnOrLine from './variable-entities-by-column-or-line.js';
+
+export const rules = {
+  'add-vue-extension': addVueExtension,
+  'import-entities-by-column-or-line': importEntitesByColumnOrLine,
+  'prefer-true-attribute-shorthand': preferTrueAttributeShorthand,
+  'use-shortest-alias': useShortestAlias,
+  'variable-entities-by-column-or-line': variableEntitiesByColumnOrLine,
+};
+
+export default { rules };
